a13cbe7ef84300afa9fc7f5168372d8707fdd525
Alexy Gallardo Segura <alexy.gallardo@yandex.com>
2024-05-22 08:52:21 -0400

    add middleware to sync data to localstorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import todosReducer from '../features/todos/todoSlice'
 import filtersReducer from '../features/filters/filtersSlice'
 
+const syncToLocalStorageMiddleware = state => next => action => {
+  const result = next(action)
+  localStorage.setItem('filters', state.getState().filters)
+  localStorage.setItem('todos', JSON.stringify(state.getState().todos))
+  return result
+}
+
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(syncToLocalStorageMiddleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
